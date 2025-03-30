6c5802c5caa8bd8c241dcc7af550193c208147c7
Bruce Bookman <bruce.bookman@gmail.com>
2025-03-30 10:35:12 -0400

    added config.  removed from gitignore

---
+++ config.py
@@ -0,0 +1,4 @@
+# Configuration for the API
+base_url = "https://api.bee.computer"
+api_key = "sk-eb1593140ffde5856d6e96d9f9c4a74eb20d4441ea84e6e2"
+polling_interval = 1  # Polling interval in hours
